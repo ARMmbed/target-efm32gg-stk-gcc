e79f3e49ff8ed0ab4cf4832aa0489f0ac3319349
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2016-02-24 10:18:42 +0100

    Remove debug options; should be provided by target-mbed-gcc

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -15,12 +15,12 @@ add_definitions("-DEFM32GG990F1024 -DEFM32GG -DEFM32")
 
 # append non-generic flags, and set EFM32GG-specific link script
 
-set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m3 -mthumb -D__thumb2__ -g -gdwarf-3 -g3 -gstrict-dwarf")
+set(_CPU_COMPILATION_OPTIONS "-mcpu=cortex-m3 -mthumb -D__thumb2__")
 
 set(CMAKE_C_FLAGS_INIT             "${CMAKE_C_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
 set(CMAKE_ASM_FLAGS_INIT           "${CMAKE_ASM_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
 set(CMAKE_CXX_FLAGS_INIT           "${CMAKE_CXX_FLAGS_INIT} ${_CPU_COMPILATION_OPTIONS}")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_MODULE_LINKER_FLAGS_INIT} -mcpu=cortex-m3 -mthumb")
-set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m3 -mthumb -u _printf_float -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/efm32gg.ld\"")
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "${CMAKE_EXE_LINKER_FLAGS_INIT} -mcpu=cortex-m3 -mthumb -T\"${CMAKE_CURRENT_LIST_DIR}/../ld/efm32gg.ld\"")
 
 
